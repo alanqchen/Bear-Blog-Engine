e2b971e2a67a89fdb8c74bb0a613b4a4dc66e885
Alan Chen <alanchen1908@gmail.com>
2020-01-15 00:12:20 -0500

    Added mongodb connection and stored connection values in system environment

--- backend/go.mod
+++ backend/go.mod
@@ -3,7 +3,17 @@ module github.com/alanqchen/MGBlog/backend
 go 1.13
 
 require (
+	github.com/DataDog/zstd v1.4.4 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gorilla/mux v1.7.3
 	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.2.1
+	golang.org/x/crypto v0.0.0-20200109152110-61a87790db17 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/text v0.3.2 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -4,14 +4,19 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
-	models "github.com/alanqchen/MGBlog/backend/models"
-	"github.com/gorilla/mux"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
 	"time"
+
+	models "github.com/alanqchen/MGBlog/backend/models"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 /*
@@ -76,10 +81,59 @@ func deletePost(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(posts)
 }
 
+type key string
+
+const (
+	hostKey     = key("hostKey")
+	usernameKey = key("usernameKey")
+	passwordKey = key("passwordKey")
+	databaseKey = key("databaseKey")
+)
+
+func configDB(ctx context.Context) (*mongo.Database, error) {
+	uri := fmt.Sprintf(`mongodb+srv://%s:%s@%s/%s`,
+		ctx.Value(usernameKey).(string),
+		ctx.Value(passwordKey).(string),
+		ctx.Value(hostKey).(string),
+		ctx.Value(databaseKey).(string),
+	)
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, fmt.Errorf("todo: couldn't connect to mongo: %v", err)
+	}
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("todo: mongo client couldn't connect with background context: %v", err)
+	} else {
+		fmt.Println("Successfully connected to mongo")
+	}
+	ctx, _ = context.WithTimeout(context.Background(), 2*time.Second)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, fmt.Errorf("todo: mongo client couldn't ping with background context: %v", err)
+	} else {
+		fmt.Println("Successfully pinged mongo")
+	}
+	todoDB := client.Database("todo")
+	return todoDB, nil
+}
+
 func main() {
 	var wait time.Duration
 	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
 	flag.Parse()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	ctx = context.WithValue(ctx, hostKey, os.Getenv("MONGO_HOST"))
+	ctx = context.WithValue(ctx, usernameKey, os.Getenv("MONGO_USERNAME"))
+	ctx = context.WithValue(ctx, passwordKey, os.Getenv("MONGO_PASSWORD"))
+	ctx = context.WithValue(ctx, databaseKey, os.Getenv("MONGO_DB_KEY"))
+	_, err := configDB(ctx)
+	if err != nil {
+		log.Fatalf("todo: database configuration failed: %v", err)
+	}
 
 	router := mux.NewRouter()
 	// Add your routes as needed
@@ -99,6 +153,8 @@ func main() {
 	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
 	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
 
+	fmt.Println("Router setup")
+
 	// Run our server in a goroutine so that it doesn't block.
 	go func() {
 		if err := srv.ListenAndServe(); err != nil {
@@ -115,11 +171,11 @@ func main() {
 	<-c
 
 	// Create a deadline to wait for.
-	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	deadlinectx, cancel := context.WithTimeout(context.Background(), wait)
 	defer cancel()
 	// Doesn't block if no connections, but will otherwise wait
 	// until the timeout deadline.
-	srv.Shutdown(ctx)
+	srv.Shutdown(deadlinectx)
 	// Optionally, you could run srv.Shutdown in a goroutine and block on
 	// <-ctx.Done() if your application should wait for other services
 	// to finalize based on context cancellation.
