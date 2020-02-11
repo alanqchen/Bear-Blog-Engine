3be5c8390bace83243425fc594fc62980b8b1d52
Alan Chen <alanchen1908@gmail.com>
2020-02-11 11:27:39 -0500

    Added test 404 page, will have to fix image trying to load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Player } from 'video-react';
+import 
+{ 
+  BrowserRouter,
+  Switch, 
+  Route,
+  Redirect
+} from "react-router-dom";
+import MetaTags from "react-meta-tags";
+import FourZeroFour from "./pages/404/404";
+import "./universal/style.css"
 
 const videoPlayerStyle = {
   width: '50%',
@@ -10,12 +20,10 @@ const videoPlayerStyle = {
 
 function App() {
   return (
+    /*
     <div className="App">
       <header className="App-header">
-        <link
-        rel="stylesheet"
-        href="https://video-react.github.io/assets/video-react.css"
-        />
+        <link rel="stylesheet" href="https://video-react.github.io/assets/video-react.css"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -24,6 +32,9 @@ function App() {
         <div style={videoPlayerStyle}> 
           <Player>
             <source src="http://localhost:8080/assets/videos/2020-02-02_19-44-25_cef9198b0ac10f7d7e1583629f1825ed.mp4"/>
+          </Player>
+          <Player>
+            <source src="http://localhost:8080/assets/videos/2020-02-03_02-29-05_f094542e103332456bd5492648920bf6.mp4"/>
           </Player>
         </div>
         <a
@@ -36,6 +47,25 @@ function App() {
         </a>
       </header>
     </div>
+    */
+   <BrowserRouter>
+      <div>
+
+        <MetaTags>
+          <title>Mindup</title>
+          <meta name="description" content="Bear Post" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </MetaTags>
+
+        { /* ReactRouter offers support for putting a <nav> element here, should investigate that at some point. */ }
+
+        <Switch>
+          <Route path="/">
+            <FourZeroFour />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/404/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../universal/style.css";
+
+export default function FourZeroFour()
+{
+    return (
+        <div>
+                <div id="errorPage">
+                    <h1>404</h1>
+                    <h2>Sorry! Page not found!</h2>
+                </div> 
+        </div>
+    )
+}
