1166b1c38736f4d9c7b4067aa11ecc42ff9a1207
Alan Chen <alanchen1908@gmail.com>
2020-02-13 11:17:32 -0500

    Added image to 404

--- frontend/src/pages/404/404.js
+++ frontend/src/pages/404/404.js
@@ -7,6 +7,7 @@ export default function FourZeroFour()
     return (
         <div class="full-height">
             <div id="errorPage" class="full-height">
+                <img id="logo" src="http://localhost:8080/assets/images/2020-02-12_10-27-20_67b2b8a5159c63d082c5c4145d505749.png" alt="blog-logo"/>
                 <h1>404</h1>
                 <h2>Sorry! Page not found!</h2>
             </div> 
