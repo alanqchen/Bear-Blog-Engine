d9214311b52774d7f2cd82e06866e831791f5ccf
Alan Chen <alanchen1908@gmail.com>
2020-07-11 22:45:09 -0400

    Add passHref prop to Link component

--- frontend/components/Posts/Page/page.js
+++ frontend/components/Posts/Page/page.js
@@ -2,28 +2,23 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import { makeStyles } from '@material-ui/core/styles';
 import Layout from '../../PublicLayout/publicLayout';
-import fetch from 'isomorphic-unfetch'
-import dynamic from 'next/dynamic'
+import fetch from 'isomorphic-unfetch';
+import dynamic from 'next/dynamic';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import React, { Component } from 'react';
-import SCtheme from '../../../assets/theme/SCtheme'
-import {PostCard} from './PostCard/postCard'
-
-const PostCardLink = React.forwardRef((props, ref) => (
-<a ref={ref} {...props}>
-    
-</a>
-))
+import SCtheme from '../../../assets/theme/SCtheme';
+import { PostCard } from './PostCard/postCard';
+import { PostCardLink } from './pageStyled';
 
 function Pagination({posts}) {
     return (
         <>
             {posts.map(post => (
             <React.Fragment key={post.id}>
-            <Link href="/[year]/[month]/[slug]" as={`/${post.slug}`}>
+            <Link href="/[year]/[month]/[slug]" as={`/${post.slug}`} passHref>
                 <PostCardLink post={post}>
                     <PostCard post={post} skeleton={false}></PostCard>
                 </PostCardLink>
---
+++ frontend/components/Posts/Page/pageStyled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const PostCardLinkBase = React.forwardRef((props, ref) => (
+    <a ref={ref} {...props}>
+        
+    </a>
+))
+
+export const PostCardLink = styled(PostCardLinkBase)`
+    text-decoration: none;
+`;
+    
