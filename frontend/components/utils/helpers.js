755cf32e779e5cc843968f7fcb503fb747ea3523
Alan Chen <alanchen1908@gmail.com>
2020-07-27 20:13:50 -0400

    Clean up date function

--- frontend/components/utils/helpers.js
+++ frontend/components/utils/helpers.js
@@ -2,6 +2,6 @@ import dateFormat from 'dateformat'
 
 // Returns MMMM DD, YYYY
 export function timestamp2date(dateStr) {
-    dateStr = dateStr.slice(0, dateStr.length-3) + dateStr.slice(dateStr.length-2);
+    dateStr = dateStr.slice(0, 10);
     return dateFormat(dateStr, "mmmm") + " " + dateFormat(dateStr, "dd") + ", " + dateFormat(dateStr, "yyyy");
 }
