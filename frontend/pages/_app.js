573ace81d94f432634582fe8509439d9a5cc5226
Alan Chen <alanchen1908@gmail.com>
2020-07-27 22:40:20 -0400

    Test iSSG and fix crossOrigin

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -27,8 +27,8 @@ const App = ({Component, pageProps, router}) => {
             <Head>
                 <title>Bear Post</title>
                 <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-                <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
-                <link rel="preconnect" href={config.apiURL} crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="true" />
+                <link rel="preconnect" href={config.apiURL} crossOrigin="true" />
             </Head>
             <MUIThemeProvider theme={theme}>
                 <ThemeProvider theme={SCtheme}>
