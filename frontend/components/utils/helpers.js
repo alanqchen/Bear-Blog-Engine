a3653b40a9376748135873ce8700769906cb29ba
Alan Chen <alanchen1908@gmail.com>
2020-09-01 02:21:53 -0400

    Fix minor date issue

--- frontend/components/utils/helpers.js
+++ frontend/components/utils/helpers.js
@@ -2,6 +2,10 @@ import dateFormat from 'dateformat'
 
 // Returns MMMM DD, YYYY
 export function timestamp2date(dateStr) {
+    // YYYY-MM-DD
     dateStr = dateStr.slice(0, 10);
-    return dateFormat(dateStr, "mmmm") + " " + dateFormat(dateStr, "dd") + ", " + dateFormat(dateStr, "yyyy");
+    // YYYY-MM-DDT00:00:00
+    dateStr += "T00:00:00";
+    const date = new Date(dateStr.replace(/-/g, '\/').replace(/T.+/, ''));
+    return dateFormat(date, "mmmm") + " " + dateFormat(date, "dd") + ", " + dateFormat(date, "yyyy");
 }
