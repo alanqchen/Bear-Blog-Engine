3e724b6a04b32daedeb387c71851910c9ca2a2a9
Alan Chen <alanchen1908@gmail.com>
2020-10-20 19:11:52 -0400

    Remove jss-server-side on client

--- frontend/pages/auth/portal/dashboard/post/[year]/[month]/[slug]/index.js
+++ frontend/pages/auth/portal/dashboard/post/[year]/[month]/[slug]/index.js
@@ -1,7 +1,10 @@
+import { useRouter } from "next/router";
 import Layout from "../../../../../../../../components/DashboardLayout/dashboardLayout";
 import EditorWrapper from "../../../../../../../../components/Editor/EditorWrapper";
 
 const Index = ({ query }) => {
+  const router = useRouter();
+  console.log(router.query);
   return (
     <Layout selectedCategory={"None"}>
       <EditorWrapper query={query} />
